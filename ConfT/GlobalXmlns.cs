98b98795ac93d351a3879ffa0335f921b0c99787
Rudiak <romain.mottard@gmail.com>
2025-12-11 12:55:33 +0100

    initial commit electron

--- ConfT/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ConfT")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ConfT.Pages")]
--- README.md
+++
@@ -1,2 +0,0 @@
-This is a project aiming to revive SDN apps.
-Work in progress, nothing usable yet.
